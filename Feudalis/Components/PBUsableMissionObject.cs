5c54f22373d5d4c16f058920feecb726efaf8b12
David Pina <dpina@edu.tecnocampus.cat>
2023-09-07 18:10:25 +0200

    Add PBUsableMissionObject

---
+++ Feudalis/Components/PBUsableMissionObject.cs
@@ -0,0 +1,22 @@
+﻿using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace Feudalis.Components
+{
+    public abstract class PBUsableMissionObject : UsableMissionObject
+    {
+
+        public int MaxUseDistance = 50;
+
+        public override bool IsDisabledForAgent(Agent agent)
+        {
+            Vec3 objectLocation = GameEntity.GlobalPosition;
+            Vec3 agentLocation = agent?.Position ?? Vec3.Invalid;
+
+            float distance = objectLocation.DistanceSquared(agentLocation);
+
+            return distance <= this.MaxUseDistance && base.IsDisabledForAgent(agent);
+        }
+
+    }
+}
